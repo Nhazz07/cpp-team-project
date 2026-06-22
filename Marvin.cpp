cb43b437b6404a69a3d808d44a377ca2bacbead9
Sakhey Marvin <sakheymavin@gmail.com>
2026-06-22 11:34:14 +0700

    added score in marvin.cpp

--- Marvin.cpp
+++ Marvin.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main() {
     int age = 20;
+    double score = 90;
     cout << "Hello, this is Marvin! Branch!" << endl;
     cout << age << endl;
+    cout << score << endl;
     return 0;
 }
